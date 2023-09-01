cbb126ec8bbe1ee3bdaaac3de0cfd369c1fb389c
lino <michi.banfi01@gmail.com>
2023-09-01 17:54:24 +0200

    sium

--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -25,6 +25,8 @@ import com.swmansion.reanimated.ReanimatedPackage;
 import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 // react-native-screens
 import com.swmansion.rnscreens.RNScreensPackage;
+// react-native-splash-screen
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 // react-native-svg
 import com.horcrux.svg.SvgPackage;
 // react-native-vector-icons
@@ -81,6 +83,7 @@ public class PackageList {
       new ReanimatedPackage(),
       new SafeAreaContextPackage(),
       new RNScreensPackage(),
+      new SplashScreenReactPackage(),
       new SvgPackage(),
       new VectorIconsPackage()
     ));
--- src/navigation/bottom_tabs.tsx
+++ src/navigation/bottom_tabs.tsx
@@ -1,79 +1,121 @@
-import { Pressable, Text, View } from "react-native"
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import {Pressable, Text, View} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/Ionicons';
-import React, { FC } from "react"
-import { Download } from "../screens/download"
-import { Route } from "../screens/route"
-import { colors } from "../utils/colors";
-import { Settings } from "../screens/settings";
-import { MapStack } from "./map_stack";
-import { SettingsStack } from "./settings_stack";
+import React, {FC} from 'react';
+import {Download} from '../screens/download';
+import {Route} from '../screens/route/route';
+import {RouteStack} from './route_stack';
+import {colors} from '../utils/colors';
+import {Settings} from '../screens/settings';
+import {MapStack} from './map_stack';
+import {SettingsStack} from './settings_stack';
 
 interface INavItem {
-    props: any
-    name: string
-    icon: string
+  props: any;
+  name: string;
+  icon: string;
 }
 
 const NavItem: FC<INavItem> = ({props, icon}) => {
-    return (
-        <Pressable {...props}>
-            <View style={{width:"70%", height:"100%", alignItems:"center", justifyContent:"center"}}>
-                <Icon name={icon} color={props?.accessibilityState?.selected ? colors.primary : colors.light} size={30}/>
-            </View>
-        </Pressable>
-    )
-}
-
+  return (
+    <Pressable {...props}>
+      <View
+        style={{
+          width: '70%',
+          height: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Icon
+          name={icon}
+          color={
+            props?.accessibilityState?.selected ? colors.primary : colors.light
+          }
+          size={30}
+        />
+      </View>
+    </Pressable>
+  );
+};
 
 export const BottomTabsNavigation = () => {
-    const Tab = createBottomTabNavigator()
+  const Tab = createBottomTabNavigator();
 
-    return (
-        <Tab.Navigator
-            screenOptions={{
-                headerShown: false,
-                tabBarStyle: { height: 70, borderTopWidth: 0},
-
-            }}
-            sceneContainerStyle={{backgroundColor:colors.secondary}}
-        >
-            <Tab.Screen
-                options={{
-                    tabBarButton: (props) => <NavItem props={props} name={"Mappa"} icon={props?.accessibilityState?.selected ? "map" : "map-outline"}/>,
-                    tabBarInactiveTintColor: colors.light,
-                    tabBarActiveTintColor: colors.primary,
-                }}
-                name="MapStack"
-                component={MapStack}
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {height: 70, borderTopWidth: 0},
+      }}
+      sceneContainerStyle={{backgroundColor: colors.secondary}}>
+      <Tab.Screen
+        options={{
+          tabBarButton: props => (
+            <NavItem
+              props={props}
+              name={'Mappa'}
+              icon={props?.accessibilityState?.selected ? 'map' : 'map-outline'}
             />
-            <Tab.Screen
-                options={{
-                    tabBarButton: (props) => <NavItem props={props} name={"Route"} icon={props?.accessibilityState?.selected ? "trail-sign" : "trail-sign-outline"}/>,
-                    tabBarInactiveTintColor: colors.light,
-                    tabBarActiveTintColor: colors.primary,
-                }}
-                name="Route"
-                component={Route}
+          ),
+          tabBarInactiveTintColor: colors.light,
+          tabBarActiveTintColor: colors.primary,
+        }}
+        name="MapStack"
+        component={MapStack}
+      />
+      <Tab.Screen
+        options={{
+          tabBarButton: props => (
+            <NavItem
+              props={props}
+              name={'Route'}
+              icon={
+                props?.accessibilityState?.selected
+                  ? 'trail-sign'
+                  : 'trail-sign-outline'
+              }
             />
-            <Tab.Screen
-                options={{
-                    tabBarButton: (props) => <NavItem props={props} name={"Download"} icon={props?.accessibilityState?.selected ? "arrow-down-circle" : "arrow-down-circle-outline"} />,
-                    tabBarInactiveTintColor: colors.light,
-                    tabBarActiveTintColor: colors.primary,
-                }}
-                name="Download"
-                component={Download}
+          ),
+          tabBarInactiveTintColor: colors.light,
+          tabBarActiveTintColor: colors.primary,
+        }}
+        name="Route"
+        component={RouteStack}
+      />
+      <Tab.Screen
+        options={{
+          tabBarButton: props => (
+            <NavItem
+              props={props}
+              name={'Download'}
+              icon={
+                props?.accessibilityState?.selected
+                  ? 'arrow-down-circle'
+                  : 'arrow-down-circle-outline'
+              }
             />
-            <Tab.Screen
-                options={{
-                    tabBarButton: (props) => <NavItem props={props} name={"Settings"} icon={props?.accessibilityState?.selected ? "cog" : "cog-outline"} />,
-                    tabBarInactiveTintColor: colors.light,
-                    tabBarActiveTintColor: colors.primary,
-                }}
-                name="SettingsStack"
-                component={SettingsStack}
+          ),
+          tabBarInactiveTintColor: colors.light,
+          tabBarActiveTintColor: colors.primary,
+        }}
+        name="Download"
+        component={Download}
+      />
+      <Tab.Screen
+        options={{
+          tabBarButton: props => (
+            <NavItem
+              props={props}
+              name={'Settings'}
+              icon={props?.accessibilityState?.selected ? 'cog' : 'cog-outline'}
             />
-        </Tab.Navigator>
-    )
-}
+          ),
+          tabBarInactiveTintColor: colors.light,
+          tabBarActiveTintColor: colors.primary,
+        }}
+        name="SettingsStack"
+        component={SettingsStack}
+      />
+    </Tab.Navigator>
+  );
+};
