8629767a83b40ffe15160e1090a63c3c94289621
michelebanfi <michi.banfi01@gmail.com>
2023-09-12 12:00:40 +0200

    about us

--- src/screens/settings.tsx
+++ src/screens/settings.tsx
@@ -34,7 +34,9 @@ export const Settings = ({navigation}) => {
           isEnd
           name={tra('impostazioni.chi')}
           icon={'heart-circle-outline'}
-          onPress={undefined}
+          onPress={() => {
+            navigation.navigate('AboutUs');
+          }}
         />
       </View>
     </PrincipalWrapper>
--- src/translations/resources/en.ts
+++ src/translations/resources/en.ts
@@ -1,21 +1,27 @@
 export default {
-    mappa:{
-        abilitaGeo: "Please enable geolocalization!"
-    },
-    search:{
-        cerca: "Search places"
-    },
-    download:{
-        titolo: "Offline hikes",
-        titoloModal: "Delete saved hike",
-        seiSicuro: "Are you sure you want to delete the saved hike? Once confirmed you will not be able to view it offline",
-        conferma: "Confirm",
-        annulla: "Cancel"
-    },
-    impostazioni:{
-        titolo: "Settings",
-        gestisci: "Manage downloaded maps",
-        lingua: "Language",
-        chi: "About us"
-    },
+  mappa: {
+    abilitaGeo: 'Please enable geolocalization!',
+  },
+  search: {
+    cerca: 'Search places',
+  },
+  download: {
+    titolo: 'Offline hikes',
+    titoloModal: 'Delete saved hike',
+    seiSicuro:
+      'Are you sure you want to delete the saved hike? Once confirmed you will not be able to view it offline',
+    conferma: 'Confirm',
+    annulla: 'Cancel',
+  },
+  impostazioni: {
+    titolo: 'Settings',
+    gestisci: 'Manage downloaded maps',
+    lingua: 'Language',
+    chi: 'About us',
+    aboutus:
+      'Hi, we are Ste and Mic we\'re two friends that who didnt had nothing to do during august and we decided to create an app to go hiking\
+                Ste didn\'t care much, but Mic thought it was useful, and here we are.\n\
+                PS: We spent too much time on the design of this app.. hence, if you dont like it, dont tell us.\n\
+                PPS: We really like "Brasato" (a sort of stew very popular in north italy), so, if you want to make a present for us dont hesisate to contact us, 🥘 <3',
+  },
 };
